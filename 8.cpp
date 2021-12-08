81ae2b5155affbe9bb752c148113a6c2cc0205e8
Tristan Phillips <tkphillips@crimson.ua.edu>
2021-12-08 17:05:49 -0600

    very close solution

--- 8.cpp
+++ 8.cpp
@@ -33,5 +33,138 @@ int main() {
         }
     }
     cout << count << endl;
+    //part 2
+    int sum = 0;
+    for(int i = 0; i < 200; i++){
+        string decoded[10] = {""};
+        vector<string> toDecode;
+        for(int j = 0; j < 10; j++){
+            toDecode.push_back(input[i][j]);
+        }
+        //find unique length digits => 1,4,7,8
+        for(int n = 0; n < toDecode.size(); n++){
+            if(toDecode[n].length() == 2){
+                decoded[1] = toDecode[n];
+                toDecode.erase(toDecode.begin() + n);
+                n--;
+            }
+            if(toDecode[n].length() == 3){
+                decoded[7] = toDecode[n];
+                toDecode.erase(toDecode.begin() + n);
+                n--;
+            }
+            if(toDecode[n].length() == 4){
+                decoded[4] = toDecode[n];
+                toDecode.erase(toDecode.begin() + n);
+                n--;
+            }
+            if(toDecode[n].length() == 7){
+                decoded[8] = toDecode[n];
+                toDecode.erase(toDecode.begin() + n);
+                n--;
+            }
+        }
+        //find one with six digits that includes all the one digits => 0
+        for(int n = 0; n < toDecode.size(); n++){
+            bool found = true;
+            if(toDecode[n].length() == 6){
+                for(int j = 0; j < 2; j++){
+                    if (toDecode[n].find(decoded[1][j]) == std::string::npos){
+                        found = false;
+                    }
+                }
+                if(found){
+                    decoded[0] = toDecode[n];
+                    toDecode.erase(toDecode.begin() + n);
+                    break;
+                } 
+            }
+        }
+        //find one off from 8 => 6
+        for(int n = 0; n < toDecode.size(); n++){
+            int notFoundCount = 0;
+            if(toDecode[n].length() == 6){
+                for(int j = 0; j < 7; j++){
+                    if (toDecode[n].find(decoded[8][j]) == std::string::npos){
+                        notFoundCount++;
+                    }
+                }
+                if(notFoundCount == 1){
+                    decoded[6] = toDecode[n];
+                    toDecode.erase(toDecode.begin() + n);
+                    break;
+                } 
+            }
+        }
+        //find the other with six digits => 9
+        for(int n = 0; n < toDecode.size(); n++){
+            if(toDecode[n].length() == 6){
+                decoded[9] = toDecode[n];
+                toDecode.erase(toDecode.begin() + n);
+                break;
+            }
+        }
+        //find one with five digits and all of 1 => 3
+        for(int n = 0; n < toDecode.size(); n++){
+            bool found = true;
+            if(toDecode[n].length() == 5){
+                for(int j = 0; j < 2; j++){
+                    if (toDecode[n].find(decoded[1][j]) == std::string::npos){
+                        found = false;
+                    }
+                }
+                if(found){
+                    decoded[3] = toDecode[n];
+                    toDecode.erase(toDecode.begin() + n);
+                    break;
+                } 
+            }
+        }
+        //find one with five digits with only one segment differnent from 3 => 2
+        for(int n = 0; n < toDecode.size(); n++){
+            int notFoundCount = 0;
+            if(toDecode[n].length() == 5){
+                for(int j = 0; j < 2; j++){
+                    if (toDecode[n].find(decoded[3][j]) == std::string::npos){
+                        notFoundCount++;
+                    }
+                }
+                if(notFoundCount == 1){
+                    decoded[2] = toDecode[n];
+                    toDecode.erase(toDecode.begin() + n);
+                    break;
+                } 
+            }
+        }
+        //process of elimination => 5
+        decoded[5] = toDecode.at(0);
+        toDecode.erase(toDecode.begin());
+
+        for(int g = 0; g < 10; g++){
+            cout << g << ": " << decoded[g] << endl;
+        }
+        //decode last 4
+        int num[4] = {0};
+        for(int n = 11; n < 15; n++){
+            int numSum = 0;
+            for(int v = 0; v < input[i][n].length(); v++){
+                numSum += input[i][n][v];
+            }
+            for(int f = 0; f < 10; f++){
+                int decodedSum = 0;
+                for(int v = 0; v < decoded[f].length(); v++){
+                    decodedSum += decoded[f][v];
+                }
+                if(numSum == decodedSum){
+                    num[n-11] = f;
+                    break;
+                }
+            }
+        }
+        int number = num[3] + (10 * num[2]) + (100 * num[1]) + (1000 * num[0]);
+        cout << number << endl;
+        sum += number;
+    }
+    cout << sum << endl;
     return 0;
 }
